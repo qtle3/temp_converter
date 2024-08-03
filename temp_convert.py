af90f768e71ac0dbf7801fa033a837b1deb3054b
Quocviet Le <quocvietlet@gmail.com>
2024-08-03 18:32:32 -0500

    comments

--- temp_convert.py
+++ temp_convert.py
@@ -1,14 +1,17 @@
-# convert2.py
 #      A program to convert Celsius temps to Fahrenheit.
 #      This version issues heat and cold warnings.
+
 
 def main():
     celsius = float(input("What is the Celsius temperature? "))
     fahrenheit = (9 / 5) * celsius + 32
     print("The temperature is", fahrenheit, "degrees fahrenheit.")
+    # Print warnings for extreme temps
     if fahrenheit > 90:
         print("It's really hot out there, be careful!")
-    if fahrenheit < 30:
+    # Print warnings for extreme cold temps
+    elif fahrenheit < 30:
         print("Brrrrr. Be sure to dress warmly")
+
 
 main()
